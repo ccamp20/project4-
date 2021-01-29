83330e42eea03880fdf20c656f494b7cb8406ec8
cali camp :) <calista.camp@patelhs.org>
2021-01-29 17:21:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please enter your current age");
+    int ageNow = scan.nextInt();
+    int univAge = 18;
+
+    System.out.println(yearsLeft(univAge,ageNow));
+    
+    static String yearsLeft(int univAge, int ageNow)
+    {
+      int yearsLeft = univAge - ageNow;
+      String years = "You have " + yearsLeft + "years left until college";
+      return years;
+    }
+  }
+}
